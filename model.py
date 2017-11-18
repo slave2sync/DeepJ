5f3f2f2235aea2e178aac524a3a108828f72031a
Henry Mao <admin@calclavia.com>
2017-11-17 21:56:04 -0800

    Fixed GRU hidden batch size

--- model.py
+++ model.py
@@ -14,7 +14,7 @@ class DeepJ(nn.Module):
     """
     The DeepJ neural network model architecture.
     """
-    def __init__(self, num_units=1024, num_layers=3, style_units=32):
+    def __init__(self, num_units=512, num_layers=3, style_units=32):
         super().__init__()
         self.num_units = num_units
         self.num_layers = num_layers
@@ -48,16 +48,11 @@ class DeepJ(nn.Module):
             states = [None for _ in range(self.num_layers)]
 
         for l, rnn in enumerate(self.rnns):
-            all_h = []
-            for t in range(seq_len):
-                h = rnn(x[:, t], states[l])
-                states[l] = h
-                all_h.append(h)
-                # Style integration
-                # x = x + style[:, l * self.num_units:(l + 1) * self.num_units].unsqueeze(1).expand(-1, seq_len, -1)
+            x = rnn(x, states[l])
+            states[l] = x
+            # Style integration
+            # x = x + style[:, l * self.num_units:(l + 1) * self.num_units].unsqueeze(1).expand(-1, seq_len, -1)
         
-            x = torch.stack(all_h, dim=1)
-
         x = self.output_linear(x)
         return x, states
 
@@ -96,20 +91,30 @@ class RNNCell(nn.Module):
         for weight in self.parameters():
             weight.data.uniform_(-stdv, stdv)
 
-    def forward(self, input, hidden):
+    def forward(self, x, hidden):
+        batch_size = x.size(0)
+        seq_len = x.size(1)
+
         if hidden is None:
-            hidden = var(torch.zeros(1, self.hidden_size))
+            hidden = var(torch.zeros(batch_size, self.hidden_size))
 
-        gi = self.l1(F.linear(input, self.w_ih, self.b_ih))
-        gh = self.l2(F.linear(hidden, self.w_hh, self.b_hh))
-        i_r, i_i, i_n = gi.chunk(3, 1)
-        h_r, h_i, h_n = gh.chunk(3, 1)
+        all_h = []
+        for t in range(seq_len):
+            # GRU Section #
+            gi = self.l1(F.linear(x[:, t], self.w_ih, self.b_ih))
+            gh = self.l2(F.linear(hidden, self.w_hh, self.b_hh))
+            i_r, i_i, i_n = gi.chunk(3, 1)
+            h_r, h_i, h_n = gh.chunk(3, 1)
 
-        resetgate = F.sigmoid(i_r + h_r)
-        inputgate = F.sigmoid(i_i + h_i)
-        newgate = F.tanh(self.l3(i_n) + resetgate * self.l4(h_n))
-        hy = newgate + inputgate * (hidden - newgate)
-        return hy
+            resetgate = F.sigmoid(i_r + h_r)
+            inputgate = F.sigmoid(i_i + h_i)
+            newgate = F.tanh(self.l3(i_n) + resetgate * self.l4(h_n))
+            hidden = newgate + inputgate * (hidden - newgate)
+
+            all_h.append(hidden)
+
+        all_h = torch.stack(all_h, dim=1)
+        return all_h
 
 class LayerNorm(nn.Module):
 
