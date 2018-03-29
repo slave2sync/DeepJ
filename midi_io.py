e0a59d868a6231814fcb06d2d6453286efd359b3
s2s <slave2sync@gmail.com>
2018-03-29 21:16:42 +0300

    Update midi_io.py

--- midi_io.py
+++ midi_io.py
@@ -88,7 +88,7 @@ def midi_to_seq(midi_file, track):
     print('=========================================================')
     print('midi_to_seq:midi_file', midi_file)
     print('midi_to_seq:Track {}: {}'.format(i, track.name)
-    print('midi_to_seq:ticks_per_beat {}', midi_file.ticks_per_beat)
+    #print('midi_to_seq:ticks_per_beat {}', midi_file.ticks_per_beat)
     print('=========================================================')
 
     events = []
